3df96a2d40c10aa306e6677a4ee116965a11a6f4
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-12 00:27:42 +0900

    fix: Fix cli.ts

--- cli.ts
+++ cli.ts
@@ -3,28 +3,27 @@ import { Command } from "https://deno.land/x/cliffy@v0.20.1/command/mod.ts";
 import { base85encode, base85decode } from "./mod.ts";
 
 try {
-  const stdin: string[] = [];
+  let stdin: string;
+  let filled: boolean;
   const isatty: boolean = Deno.isatty(Deno.stdin.rid);
   if (!isatty) {
-    const get: string = await getStdin({ exitOnEnter: false });
-    stdin.push(get);
+    stdin = await getStdin({ exitOnEnter: false });
+    filled = stdin ? true : false;
   }
   const { options, args } = await new Command()
     .name("base85")
-    .description("Base85 (Adobe) encode or decode standard input, to standard output.")
+    .description("** Binary input/output is not supported. **\nBase85 (Ascii85 with Adobe-Escape) encode or decode standard input, to standard output.")
     .version("0.0.3")
     .option("-d, --decode", "decode data")
     .arguments("<option>")
-    .parse(isatty ? Deno.args : [...Deno.args, ...stdin]);
+    .parse(isatty ? Deno.args : [...Deno.args, stdin!]);
   const { decode } = options;
   const runner = decode ? base85decode : base85encode;
-  if (!isatty) {
-    const result: string | Uint8Array = runner(String(args[0]));
-    if (typeof result === "string") {
-      console.log(result);
-    } else {
-      await Deno.stdout.write(result);
-    }
+  const result: string | Uint8Array = runner(String(args[0]));
+  if (filled!) {
+    typeof result === "string" ? console.log(result) : await Deno.stdout.write(result);
+  } else if (!isatty && filled!) {
+    typeof result === "string" ? console.log(result) : await Deno.stdout.write(result);
   } else {
     throw new Error("base85: No such file or directory");
   }
