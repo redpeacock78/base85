ff53735155feec92f3fc415c7e705d51751d795f
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-11 23:27:10 +0900

    fix: Fix cli.ts

--- cli.ts
+++ cli.ts
@@ -1,30 +1,30 @@
-import { readLines } from "https://deno.land/std@0.101.0/io/mod.ts";
+import { getStdin } from "https://deno.land/x/get_stdin@v1.1.0/mod.ts";
 import { Command } from "https://deno.land/x/cliffy@v0.20.1/command/mod.ts";
 import { base85encode, base85decode } from "./mod.ts";
 
 try {
-  let judge: boolean;
   const stdin: string[] = [];
   const isatty: boolean = Deno.isatty(Deno.stdin.rid);
   if (!isatty) {
-    for await (const line of readLines(Deno.stdin)) {
-      judge = typeof readLines(Deno.stdin) === "object" ? true : false;
-      stdin.push(line);
-    }
+    const get: string = await getStdin({ exitOnEnter: false });
+    stdin.push(get);
   }
   const { options, args } = await new Command()
     .name("base85")
     .description("Base85 (Adobe) encode or decode standard input, to standard output.")
-    .version("0.0.2")
+    .version("0.0.3")
     .option("-d, --decode", "decode data")
     .arguments("<option>")
-    .parse(isatty ? Deno.args : [...Deno.args, stdin.join("\n")]);
+    .parse(isatty ? Deno.args : [...Deno.args, ...stdin]);
   const { decode } = options;
   const runner = decode ? base85decode : base85encode;
-  if (judge!) {
-    console.log(runner(String(args[0])));
-  } else if (!isatty && judge!) {
-    console.log(runner(String(args[0])));
+  if (!isatty) {
+    const result: string | Uint8Array = runner(String(args[0]));
+    if (typeof result === "string") {
+      console.log(result);
+    } else {
+      await Deno.stdout.write(result);
+    }
   } else {
     throw new Error("base85: No such file or directory");
   }
