c1c44afaaa886c049784465f5d682e30592852b1
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-12 16:17:20 +0900

    chore: Change cli.ts

--- cli.ts
+++ cli.ts
@@ -13,7 +13,7 @@ try {
   const { options, args } = await new Command()
     .name("base85")
     .description("Base85 (Ascii85 with Adobe Escape Sequence) encode or decode FILE, or standard input, to standard output.")
-    .version("0.0.6")
+    .version("0.0.7")
     .option("-d, --decode", "Decode data")
     .arguments("<option>")
     .parse(isatty ? Deno.args : [...Deno.args, [...stdin!].join(",")]);
