7d670869c89a6a3125aaf2fb8233ebbc6ecdeec2
redpeacock78 <scarletpeacock78@gmail.com>
2021-11-20 18:06:54 +0900

    chore: Add deps.ts

---
+++ deps.ts
@@ -0,0 +1,5 @@
+export { readAllSync } from "https://deno.land/std@0.114.0/streams/conversion.ts";
+export { getStdinBufferSync } from "https://deno.land/x/get_stdin@v1.1.0/mod.ts";
+export { Command } from "https://deno.land/x/cliffy@v0.20.1/command/mod.ts";
+export { base85decode, base85encode } from "./mod.ts";
+export { version } from "./version.ts";
